77d5247ac72b46401173129e9f7cba39f42339d9
Houssem Ghaythi <ghaythihoussem00@gmail.com>
2026-06-18 14:41:09 +0200

    server update

--- car-backend/server.js
+++ car-backend/server.js
@@ -47,7 +47,7 @@ const corsOptions = {
 
 // middlewares
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("/*", cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req, res) => {
